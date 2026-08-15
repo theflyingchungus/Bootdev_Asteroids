38c0452f93dbeae24cd11a6727bb9409e49a4b6d
theflyingchungus <suhaibster@gmail.com>
2026-08-15 10:59:01 +0800

    added asteroid disappears when shot

--- bootdev_asteroids/main.py
+++ bootdev_asteroids/main.py
@@ -46,6 +46,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
 
         screen.fill("black")
 
--- bootdev_asteroids/player.py
+++ bootdev_asteroids/player.py
@@ -53,13 +53,13 @@ class Player(CircleShape):
         self.shot_cd = max(0, self.shot_cd - dt)
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_LEFT]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_RIGHT]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
+        if keys[pygame.K_UP]:
             self.move(dt)
-        if keys[pygame.K_s]:
+        if keys[pygame.K_DOWN]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
